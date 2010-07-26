b1ba0708761cb56193ba3097a7ce3b427540398a
Bryan Shelton <bryan@sheltonopensolutions.com>
2010-07-25 19:26:28 -0600

    Moved model

--- app.rb
+++ app.rb
@@ -6,26 +6,8 @@ require 'dm-types'
 #Load DataMapper
 DataMapper.setup(:default, YAML::load_file(File.expand_path('../config.yml',__FILE__))['database']['uri'])
 
-class Log
-  include DataMapper::Resource
-  property :id, Serial
-  property :payload, Json
-  property :repo, String
-  property :branch, String
-
-  
-  before :save, :load_data
-  
-  #Set the branch
-  def load_data
-    self.branch = self.payload['ref'].split('/')[2]
-    self.repo = self.payload['repository']['name']
-  rescue
-    self.branch = nil
-    self.repo = nil
-  end
-  
-end
+#Load our DataMapper models
+Dir[File.expand_path('../models/*.rb',__FILE__)].each {|f| require f }
 
 #This will automatically add the table
 DataMapper.auto_upgrade!
---
+++ models/log.rb
@@ -0,0 +1,21 @@
+#Log
+class Log
+  include DataMapper::Resource
+  property :id, Serial
+  property :payload, Json
+  property :repo, String
+  property :branch, String
+
+  
+  before :save, :load_data
+  
+  #Set the branch
+  def load_data
+    self.branch = self.payload['ref'].split('/')[2]
+    self.repo = self.payload['repository']['name']
+  rescue
+    self.branch = nil
+    self.repo = nil
+  end
+  
+end
